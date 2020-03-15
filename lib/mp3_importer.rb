f1644f980144a5ebf04f0833a415b644cab589a4
Ebisiemen Ajayi <ebisiemen@gmail.com>
2020-03-15 21:39:11 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -14,9 +14,8 @@ class MP3Importer
     path_with_mp3 = Dir[@path+"/*.mp3"]
     path_with_mp3.map {|element| element.split("/")[-1]}
   end   
-end   
 
  def import
    files.each {|file| Song.new_by_filename(file)}
-  end 
- end      
+ end 
+end      
